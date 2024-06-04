60628e094e2329fc657e068f2fa2ccee659d4d02
Brendajacome <bcjacomea@uce.edu.ec>
2024-06-03 22:13:37 -0500

    Update HelloApplication.java

--- src/main/java/com/example/orderrgb/HelloApplication.java
+++ src/main/java/com/example/orderrgb/HelloApplication.java
@@ -7,29 +7,25 @@ public class HelloApplication {
         Scanner value = new Scanner(System.in);
         System.out.print("Enter the value of R (between 0 and 255): ");
         int R = value.nextInt();
-        System.out.print("Enter the value of G (between 0 and 255): ");
-        int G = value.nextInt();
-        System.out.print("Enter the value of B (between 0 and 255): ");
-        int B = value.nextInt();
+        if (R < 0 || R > 255) {
+            System.out.println("Value of R is out of range.");
+            System.exit(0);
+        }
 
-        if (R < 0) {
-            R = 0;
+        System.out.print("Enter the value of G (between 0 and 255):");
+        int G = value.nextInt();
+        if (G < 0 || G > 255) {
+            System.out.println("Value of G is out of range.");
+            System.exit(0);
         }
-        if (R > 255) {
-            R = 255;
+
+        System.out.print("Enter the value of B (between 0 and 255):");
+        int B = value.nextInt();
+        if (B < 0 || B > 255) {
+            System.out.println("Value of B is out of range.");
+            System.exit(0);
         }
-        if (G < 0) {
-            G = 0;
-        }
-        if (G > 255) {
-            G = 255;
-        }
-        if (B < 0) {
-            B = 0;
-        }
-        if (B > 255) {
-            B = 255;
-        }
+
         System.out.println("The RGB color code is: (" + R + ", " + G + ", " + B + ")");
     }
 }
