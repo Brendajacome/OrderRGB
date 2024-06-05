4c6a0d888c03011ac18f2df7e2f6af98b8e0a857
Brendajacome <bcjacomea@uce.edu.ec>
2024-06-04 22:05:07 -0500

    Update HelloApplication.java

--- src/main/java/com/example/orderrgb/HelloApplication.java
+++ src/main/java/com/example/orderrgb/HelloApplication.java
@@ -6,22 +6,22 @@ public class HelloApplication {
     public static void main(String[] args) {
         Scanner value = new Scanner(System.in);
         System.out.print("Enter the value of R (between 0 and 255): ");
-        int R = value.nextInt();
-        if (R < 0 || R > 255) {
+        int RED = value.nextInt();
+        if (RED < 0 || RED > 255) {
             System.out.println("Value of R is out of range.");
             System.exit(0);
         }
 
         System.out.print("Enter the value of G (between 0 and 255):");
-        int G = value.nextInt();
-        if (G < 0 || G > 255) {
+        int GREEN = value.nextInt();
+        if (GREEN < 0 || GREEN > 255) {
             System.out.println("Value of G is out of range.");
             System.exit(0);
         }
 
         System.out.print("Enter the value of B (between 0 and 255):");
-        int B = value.nextInt();
-        if (B < 0 || B > 255) {
+        int BLUE = value.nextInt();
+        if (BLUE < 0 || BLUE > 255) {
             System.out.println("Value of B is out of range.");
             System.exit(0);
         }
